f884ed32b996e3d1137d4d64182f67ee7c9d5f46
Leo Palmer Sunmo <leosunmo@users.noreply.github.com>
2019-04-07 14:58:40 +1200

    [#6] Add ability to ignore dirs for `gofmt` (#15)
    * First let's get some bash going for array action
    
    * Add simple check for "GO_IGNORE_DIR" and loop to build excludes
    
    * Add the ignored dirs to the find command
    
    * Add a fancy section to add the glob at the end
    
    * Formatting
    
    * Use an eval for find command to gather files of interest once
    
    * Reuse list of files and general formatting and tidy-up

--- fmt/entrypoint.sh
+++ fmt/entrypoint.sh
@@ -1,11 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 cd "${GO_WORKING_DIR:-.}"
 
+# Build ignored directories
+IGNORED_DIRS=""
+if [ -n "${GO_IGNORE_DIRS}" ]; then
+  IGNORE_DIRS_ARR=($GO_IGNORE_DIRS)
+  for DIR in "${IGNORE_DIRS_ARR[@]}"; do
+    # If the directory doesn't end in "/*", add it
+    if [[ ! "${DIR}" =~ .*\/\*$ ]]; then
+      DIR="${DIR}/*"
+    fi
+    # Append to our list of directories to ignore
+    IGNORED_DIRS+=" -not -path \"${DIR}\""
+  done
+fi
+
+# Use an eval to avoid glob expansion
+FIND_EXEC="find . -type f -iname '*.go' ${IGNORED_DIRS}"
+
+# Get a list of files that we are interested in
+CHECK_FILES=$(eval ${FIND_EXEC})
+
 # Check if any files are not formatted.
 set +e
-test -z "$(gofmt -l -d -e $(find . -type f -iname '*.go'))"
+test -z "$(gofmt -l -d -e ${CHECK_FILES})"
 SUCCESS=$?
 set -e
 
@@ -16,16 +36,16 @@ fi
 
 # Get list of unformatted files.
 set +e
-FILES=$(sh -c "gofmt -l . $*" 2>&1)
-echo "$FILES"
+ISSUE_FILES=$(gofmt -l ${CHECK_FILES})
+echo "${ISSUE_FILES}"
 set -e
 
 # Iterate through each unformatted file.
 OUTPUT=""
-for file in $FILES; do
-DIFF=$(gofmt -d -e "$file")
+for FILE in $ISSUE_FILES; do
+DIFF=$(gofmt -d -e "${FILE}")
 OUTPUT="$OUTPUT
-\`$file\`
+\`${FILE}\`
 
 \`\`\`diff
 $DIFF
